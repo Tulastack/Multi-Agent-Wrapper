04262b93a3f8963a6efacebc22e3b5e05aeeba34
Tulastack <2009.tula@gmail.com>
2026-09-12 00:18:43 -0700

    Change cross-validation folds from 10 to 5

--- run_experiment.py
+++ run_experiment.py
@@ -74,7 +74,7 @@ def main():
 
     loo_probabilities = cross_val_predict(
         LogisticRegression(max_iter=2000, C=LOGREG_C), embeddings, labels,
-        cv=10, method="predict_proba",
+        cv=5, method="predict_proba",
     )[:, 1]
     layer2_blocked = loo_probabilities > 0.5
 
